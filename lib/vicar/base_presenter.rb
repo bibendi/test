03b684de26257baec092712706de6ae0328365bb
Меркушин Михаил <bibendi@bk.ru>
2015-06-25 22:05:58 +0500

    Update base_presenter.rb

--- lib/vicar/base_presenter.rb
+++ lib/vicar/base_presenter.rb
@@ -17,5 +17,38 @@ module Vicar
         puts 1
       end
     end
+    
+    def prepare_products_loader
+      loader.
+        select('*, (price * currency_rate) converted_price').
+        order_products_by(order_conditions).
+        order_premium_products_in_groups(order_conditions_in_groups).
+        load_sphinx_attributes(:product_groups)
+  
+      loader.load_statistics_by_words_if_empty if search?   # загружать статистику по словам для пустого поиска
+  
+      if default_listing_sort?
+        if boost_by_region?
+          loader.boost_by_region(current_region_by_ip_or_default, :with_native_regions => true) if current_region.default?
+          loader.boost_by_region(current_region) unless current_region.default?
+        end
+  
+        loader.with_regional_packet_sort(current_region) if use_virtual_packet_sort?
+      end
+  
+      if group_by_company?
+        loader.load_premium_products(:limit => CatalogueController::PRODUCTS_LIMIT) do |loader|
+          if switch_to_short_listing_mode?(loader.results)
+            @load_short_listing = true
+            @group_by_company = false
+            false # загружаем без премиальных товаров
+          else
+            true # загружаем с премиальными товарами
+          end
+        end
+      else
+        loader.load_products
+      end
+    end
   end
 end
